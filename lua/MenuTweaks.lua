eaea26c96526e24661a76c82fdf404dde24186f2
LT71 <30779314+LT71@users.noreply.github.com>
2020-02-08 10:10:45 +0100

    offline chat (#68)

--- lua/MenuTweaks.lua
+++ lua/MenuTweaks.lua
@@ -1,5 +1,16 @@
 if string.lower(RequiredScript) == "lib/managers/menumanager" then
 	function MenuCallbackHandler:get_latest_dlc_locked(...) return false end		--Hide DLC ad in the main menu
+	
+	-- Offline chat.
+	function MenuManager:toggle_chatinput()
+	    if Application:editor() or SystemInfo:platform() ~= Idstring("WIN32") or self:active_menu() or not managers.network:session() then
+		    return
+	    end
+	    if managers.hud then
+		    managers.hud:toggle_chatinput()
+		    return true
+	    end
+    end	
 
 	local modify_controls_original = MenuOptionInitiator.modify_controls
 	function MenuOptionInitiator:modify_controls(...)
