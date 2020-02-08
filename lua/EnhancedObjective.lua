470572a3230c9cb95931a08586eafde80fce5bb3
LT71 <30779314+LT71@users.noreply.github.com>
2020-02-08 11:24:18 +0100

    Subtitle options

--- lua/EnhancedObjective.lua
+++ lua/EnhancedObjective.lua
@@ -6,8 +6,43 @@ elseif string.lower(RequiredScript) == "lib/managers/hud/hudheisttimer" then
 
 -- Old
 
-elseif string.lower(RequiredScript) == "core/lib/managers/subtitle/coresubtitlepresenter" and VHUDPlus:getSetting({"HUDList", "BUFF_LIST", "show_buffs"}, true) then
+elseif string.lower(RequiredScript) == "core/lib/managers/subtitle/coresubtitlepresenter" then
 	core:module("CoreSubtitlePresenter")
+	function OverlayPresenter:show_text(text, duration)
+		self.__font_name = "fonts/font_medium_mf"
+		self._text_scale = _G.VHUDPlus:getSetting({"CustomHUD", "SCALE"}, 1)
+		local text_shadow = _G.VHUDPlus:getSetting({"CustomHUD", "SUB"}, true)
+		local label = self.__subtitle_panel:child("label") or self.__subtitle_panel:text({
+			name = "label",
+			font = self.__font_name,
+			font_size = self.__font_size * self._text_scale,
+			color = Color.white,
+			align = "center",
+			vertical = "bottom",
+			layer = 1,
+			wrap = true,
+			word_wrap = true
+		})
+		local shadow = self.__subtitle_panel:child("shadow") or self.__subtitle_panel:text({
+			name = "shadow",
+			x = 1,
+			y = 1,
+			font = self.__font_name,
+			font_size = self.__font_size * self._text_scale,
+			color = Color.black:with_alpha(1),
+			align = "center",
+			vertical = "bottom",
+			layer = 0,
+			wrap = true,
+			word_wrap = true
+		})
+		label:set_text(text)
+		shadow:set_text(text)	
+		label:set_font_size(self.__font_size * self._text_scale)
+		shadow:set_font_size(self.__font_size * self._text_scale)
+		shadow:set_visible(text_shadow)
+	end
+	
 	local _on_resolution_changed_original = OverlayPresenter._on_resolution_changed
 	function OverlayPresenter:_on_resolution_changed(...)
 		_on_resolution_changed_original(self, ...)
