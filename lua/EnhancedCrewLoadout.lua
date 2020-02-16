cc59864ce04bb2309c26ed0cd6caf36593145d5e
LT71 <30779314+LT71@users.noreply.github.com>
2020-02-16 11:03:25 +0100

    fixed the lobby

--- lua/EnhancedCrewLoadout.lua
+++ lua/EnhancedCrewLoadout.lua
@@ -141,26 +141,23 @@ if string.lower(RequiredScript) == "lib/managers/menu/contractboxgui" then
 		elseif self._loadout_visible then
 
 		else
+	        if button == Idstring("0") then
+		        local used = false
+		        local pointer = "arrow"
+		        if self._peers and SystemInfo:platform() == Idstring("WIN32") and MenuCallbackHandler:is_overlay_enabled() then
+			        for peer_id, object in pairs(self._peers) do
+				        if alive(object) and object:inside(x, y) then
+					        local peer = managers.network:session() and managers.network:session():peer(peer_id)
+					        if peer then
+						        Steam:overlay_activate("url", "http://www.steamcommunity.com/profiles/" .. peer:user_id() .. "/")
+						        return
+					        end
+				        end
+			        end
+		        end
+	        end		
 			return mouse_pressed_original(self, button, x, y, ...)
-		end
-		if not self:can_take_input() then
-		    return
-	    end
-	    if button == Idstring("0") then
-		    local used = false
-		    local pointer = "arrow"
-		    if self._peers and SystemInfo:platform() == Idstring("WIN32") and MenuCallbackHandler:is_overlay_enabled() then
-			    for peer_id, object in pairs(self._peers) do
-				    if alive(object) and object:inside(x, y) then
-					    local peer = managers.network:session() and managers.network:session():peer(peer_id)
-					    if peer then
-						    Steam:overlay_activate("url", "http://www.steamcommunity.com/profiles/" .. peer:user_id() .. "/")
-						    return
-					    end
-				    end
-			    end
-		    end
-	    end		
+		end	
 	end
 
 	function ContractBoxGui:mouse_moved(x, y, ...)
