f970564689aa230b2dd22b85b21447b84ff9e63f
LT71 <30779314+LT71@users.noreply.github.com>
2020-02-24 07:31:40 +0100

    Drill icons (#125)
    * Update Core.lua
    
    * Update OptionMenus.lua
    
    * Update Interaction.lua

--- lua/Interaction.lua
+++ lua/Interaction.lua
@@ -322,12 +322,17 @@ local custom_huds_support = VHUDPlus:getSetting({"INTERACTION", "CUSTOM_HUDS_SUP
 		self._interact_visible = nil
 		return remove_interact_original(self)
 	end
+	
+	function HUDManager:show_drill_interact(...)
+	    self._hud_interaction:show_drill_interact(...)
+    end
 
 elseif string.lower(RequiredScript) == "lib/managers/hud/hudinteraction" then
 	local init_original 				= HUDInteraction.init
 	local show_interaction_bar_original = HUDInteraction.show_interaction_bar
 	local hide_interaction_bar_original = HUDInteraction.hide_interaction_bar
 	local show_interact_original		= HUDInteraction.show_interact
+	local remove_interact_original      = HUDInteraction.remove_interact
 	local destroy_original				= HUDInteraction.destroy
 
 	local set_interaction_bar_width_original = HUDInteraction.set_interaction_bar_width
@@ -468,6 +473,82 @@ elseif string.lower(RequiredScript) == "lib/managers/hud/hudinteraction" then
 	end
 
 	function HUDInteraction:show_interact(data)
+	    local SCALE = VHUDPlus:getSetting({"INTERACTION", "TEXT_SCALE"}, 0.8)
+	    if 0 < managers.player:upgrade_level("player", "drill_speed_multiplier", 0) then
+            offset_speed = 2.25
+            offset_auto  = 1.82
+        else
+            offset_speed = 1.82		
+            offset_auto  = 2.25
+        end
+
+	    self._drill_skills_panel = self._hud_panel:panel({
+		    layer = 1,
+		    visible = true,
+		    align = "center",
+		    y = self._hud_panel:child(self._child_name_text):bottom()
+	    })
+
+	    self._drill_skills_panel:bitmap({
+		    y = 10,
+		    visible = 0 < managers.player:upgrade_level("player", "drill_speed_multiplier", 0),
+		    texture = "guis/textures/pd2/skilltree_2/icons_atlas_2",
+            texture_rect = { 3 * 80, 6 * 80, 80, 80 },
+		    color = Color.white,
+		    align = "center",
+		    w = 80 * SCALE,
+		    h = 80 * SCALE,
+		    layer = 2
+	    }):set_center_x(self._drill_skills_panel:w() / offset_speed)
+
+	    self._drill_skills_panel:bitmap({
+		    y = 10,
+		    visible = 0 < managers.player:upgrade_level("player", "drill_autorepair_2", 0),
+		    texture = "guis/textures/pd2/skilltree_2/icons_atlas_2",
+            texture_rect = { 5 * 80, 5 * 80, 80, 80 },
+		    color = Color.white,
+		    align = "center",
+		    w = 80 * SCALE,
+		    h = 80 * SCALE,
+		    layer = 2
+	    }):set_center_x(self._drill_skills_panel:w() / offset_auto)
+
+	    self._drill_skills_panel:bitmap({
+		    y = 10,
+		    visible = 0 < managers.player:upgrade_level("player", "silent_drill", 0),
+		    texture = "guis/textures/pd2/skilltree_2/icons_atlas_2",
+		    texture_rect = { 9 * 80, 6 * 80, 80, 80 },
+		    color = Color.white,
+		    align = "center",
+		    w = 80 * SCALE,
+		    h = 80 * SCALE,
+		    layer = 2
+	    }):set_center_x(self._drill_skills_panel:w() / 2 )
+
+	    if managers.player:upgrade_level("player", "silent_drill", 0) and managers.player:upgrade_level("player", "drill_autorepair_1", 0) == 1 then
+		    self._drill_skills_panel:bitmap({
+			    texture = "guis/textures/pd2/skilltree_2/ace_symbol",
+			    h = 100 * SCALE,
+			    w = 100 * SCALE,
+			    alpha = 1,
+			    blend_mode = "add",
+			    color = Color.white,
+			    layer = 1
+		    }):set_center_x(self._drill_skills_panel:w() / 2)
+	    end
+
+	    if managers.player:upgrade_level("player", "drill_speed_multiplier", 0) == 2 then
+		    self._drill_skills_panel:bitmap({
+			    texture = "guis/textures/pd2/skilltree_2/ace_symbol",
+			    h = 100 * SCALE,
+			    w = 100 * SCALE,
+			    alpha = 1,
+			    blend_mode = "add",
+			    color = Color.white,
+			    layer = 1
+		    }):set_center_x(self._drill_skills_panel:w() / offset_speed)	
+	    end
+		
 		if not VHUDPlus:getSetting({"INTERACTION", "CUSTOM_HUDS_SUPPORT"}, false) then
 			self:_rescale()
 		end
@@ -475,6 +556,18 @@ elseif string.lower(RequiredScript) == "lib/managers/hud/hudinteraction" then
 			return show_interact_original(self, data)
 		end
 	end
+	
+	function HUDInteraction:show_drill_interact()
+	    self._drill_skills_panel:set_visible(true)
+    end
+	
+	function HUDInteraction:remove_interact(...)
+	    if alive(self._drill_skills_panel) then
+		    self._drill_skills_panel:set_visible(false)
+        end
+		
+	    return remove_interact_original(self, ...)
+    end	
 
 	function HUDInteraction:destroy()
 		if self._interact_time and self._hud_panel then
@@ -517,6 +610,7 @@ elseif string.lower(RequiredScript) == "lib/managers/hud/hudinteraction" then
 elseif string.lower(RequiredScript) == "lib/units/interactions/interactionext" then
 	local _add_string_macros_original = BaseInteractionExt._add_string_macros
 	local _interact_blocked_original = IntimitateInteractionExt._interact_blocked
+	local selected_original          = BaseInteractionExt.selected	
 	local jokers = {}
 	
 	local function kill_joker()
@@ -600,6 +694,15 @@ elseif string.lower(RequiredScript) == "lib/units/interactions/interactionext" t
 			macros.VALUE = ""
 		end
 	end
+	
+	function BaseInteractionExt:selected(...)
+	    if selected_original(self, ...) and self._unit:base() and self._unit:base().is_drill and VHUDPlus:getSetting({"INTERACTION", "DRILL_ICONS"}, true) and not VHUDPlus:getSetting({"INTERACTION", "CUSTOM_HUDS_SUPPORT"}, false) then
+		    managers.hud:show_drill_interact()
+	    elseif selected_original(self, ...) and self._unit:base() and self._unit:base().is_saw and VHUDPlus:getSetting({"INTERACTION", "DRILL_ICONS"}, true) and not VHUDPlus:getSetting({"INTERACTION", "CUSTOM_HUDS_SUPPORT"}, false) then
+	        managers.hud:show_drill_interact()
+	    end
+	    return selected_original(self, ...)
+    end	
 
 elseif string.lower(RequiredScript) == "lib/managers/objectinteractionmanager" then
 	ObjectInteractionManager.AUTO_PICKUP_DELAY = VHUDPlus:getTweakEntry("AUTO_PICKUP_DELAY", "number", 0.2)	 --Delay in seconds between auto-pickup procs (0 -> as fast as possible)
