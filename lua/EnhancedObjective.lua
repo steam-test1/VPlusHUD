d080b5abdc569713e3efeaf04c56c164d38e3f5f
steam-test1 <steam-test1@users.noreply.github.com>
2020-04-09 11:56:08 +0100

    Re enabled subtitle offset

--- lua/EnhancedObjective.lua
+++ lua/EnhancedObjective.lua
@@ -43,32 +43,32 @@ elseif string.lower(RequiredScript) == "core/lib/managers/subtitle/coresubtitlep
 		shadow:set_visible(text_shadow)
 	end
 	
-	-- local _on_resolution_changed_original = OverlayPresenter._on_resolution_changed
-	-- function OverlayPresenter:_on_resolution_changed(...)
-	-- 	_on_resolution_changed_original(self, ...)
-	-- 	self:apply_bottom_offset()
-	-- end
+	local _on_resolution_changed_original = OverlayPresenter._on_resolution_changed
+	function OverlayPresenter:_on_resolution_changed(...)
+		_on_resolution_changed_original(self, ...)
+		self:apply_bottom_offset()
+	end
 
-	-- function OverlayPresenter:set_bottom(offset)
-	-- 	if self._bottom_off ~= offset then
-	-- 		self._bottom_off = offset
-	-- 		self:apply_bottom_offset()
-	-- 	end
-	-- end
+	function OverlayPresenter:set_bottom(offset)
+		if self._bottom_off ~= offset then
+			self._bottom_off = offset
+			self:apply_bottom_offset()
+		end
+	end
 
-	-- function OverlayPresenter:apply_bottom_offset()
-	-- 	if self.__subtitle_panel and self._bottom_off and _G.VHUDPlus:getSetting({"HUDList", "BUFF_LIST", "show_buffs"}, true) then
-	-- 		self.__subtitle_panel:set_height(self._bottom_off or self.__subtitle_panel:h())
-	-- 		local label = self.__subtitle_panel:child("label")
-	-- 		if label then
-	-- 			label:set_h(self.__subtitle_panel:h())
-	-- 			label:set_w(self.__subtitle_panel:w())
-	-- 		end
-	-- 		local shadow = self.__subtitle_panel:child("shadow")
-	-- 		if shadow then
-	-- 			shadow:set_h(self.__subtitle_panel:h())
-	-- 			shadow:set_w(self.__subtitle_panel:w())
-	-- 		end
-	-- 	end
-	-- end
+	function OverlayPresenter:apply_bottom_offset()
+		if self.__subtitle_panel and self._bottom_off and _G.VHUDPlus:getSetting({"HUDList", "BUFF_LIST", "show_buffs"}, true) then
+			self.__subtitle_panel:set_height(self._bottom_off or self.__subtitle_panel:h())
+			local label = self.__subtitle_panel:child("label")
+			if label then
+				label:set_h(self.__subtitle_panel:h())
+				label:set_w(self.__subtitle_panel:w())
+			end
+			local shadow = self.__subtitle_panel:child("shadow")
+			if shadow then
+				shadow:set_h(self.__subtitle_panel:h())
+				shadow:set_w(self.__subtitle_panel:w())
+			end
+		end
+	end
 end
