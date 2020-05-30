c969e6f7a18a0359cc17715e7d58017a583f9712
LT71 <30779314+LT71@users.noreply.github.com>
2020-05-30 13:34:56 +0200

    no red lasers

--- lua/NetworkHandler.lua
+++ lua/NetworkHandler.lua
@@ -11,6 +11,22 @@ function UnitNetworkHandler:sync_contour_state(unit, u_id, type, state, multipli
 	end
 end
 
+function UnitNetworkHandler:set_weapon_gadget_color(unit, red, green, blue, sender)
+	if not self._verify_character_and_sender(unit, sender) then
+		return
+	end
+
+	if red and green and blue then 
+		local threshold = 0.66
+		if red * threshold > green + blue then
+			red = 1
+			green = 51
+			blue = 1
+		end
+	end
+	unit:inventory():sync_weapon_gadget_color(Color(red / 255, green / 255, blue / 255))
+end
+
 --[[
 do return end	-- Disabled cause: WiP
 VHUDPlus.Sync = VHUDPlus.Sync or {}
