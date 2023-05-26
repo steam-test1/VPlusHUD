b561f0a5441f8cc1bb1c5c7d8a00e10501e10aa2
Vadim <129969897+vadcx@users.noreply.github.com>
2023-05-26 07:00:33 +0200

    Fix force-start crash (as a host) (#283)
    * Fix forcestart crash when game started
    
    * Refactor ForceReady into a separate file
    
    * ForceReady: only show message if force-start available
    
    * ForceReady: now uses correct name for Vanilla HUD Plus

--- devlua/Scripts.lua
+++ devlua/Scripts.lua
@@ -1,5 +1,4 @@
 if string.lower(RequiredScript) == "lib/managers/hudmanagerpd2" then
-	local set_slot_ready_orig = HUDManager.set_slot_ready
 	local set_teammate_ammo_amount_orig = HUDManager.set_teammate_ammo_amount
 
 	function HUDManager:set_teammate_ammo_amount(id, selection_index, max_clip, current_clip, current_left, max, ...)
@@ -13,65 +12,6 @@ if string.lower(RequiredScript) == "lib/managers/hudmanagerpd2" then
 		return set_teammate_ammo_amount_orig(self, id, selection_index, max_clip, current_clip, current_left, max, ...)
 	end
 
-	local FORCE_READY_CLICKS = 3
-	local FORCE_READY_TIME = 2
-	local FORCE_READY_ACTIVE_T = 90
-
-	local force_ready_start_t = 0
-	local force_ready_clicked = 0
-
-	function HUDManager:set_slot_ready(peer, peer_id, ...)
-		set_slot_ready_orig(self, peer, peer_id, ...)
-
-		if Network:is_server() and not Global.game_settings.single_player then
-			local session = managers.network and managers.network:session()
-			local local_peer = session and session:local_peer()
-			local time_elapsed = managers.game_play_central and managers.game_play_central:get_heist_timer() or 0
-			if local_peer and local_peer:id() == peer_id then
-				local t = Application:time()
-				if (force_ready_start_t + FORCE_READY_TIME) > t then
-					force_ready_clicked = force_ready_clicked + 1
-					if force_ready_clicked >= FORCE_READY_CLICKS then
-						local enough_wait_time = (time_elapsed > FORCE_READY_ACTIVE_T)
-						local friends_list = not enough_wait_time and Steam:logged_on() and Steam:friends() or {}
-						local abort = false
-						for _, peer in ipairs(session:peers()) do
-							local is_friend = false
-							for _, friend in ipairs(friends_list) do
-								if friend:id() == peer:user_id() then
-									is_friend = true
-									break
-								end
-							end
-							if not (enough_wait_time or is_friend) or not (peer:synced() or peer:id() == local_peer:id()) then
-								abort = true
-								break
-							end
-						end
-						if game_state_machine and not abort then
-							local menu_options = {
-								[1] = {
-									text = managers.localization:text("dialog_yes"),
-									callback = function(self, item)
-										managers.chat:send_message(ChatManager.GAME, local_peer, managers.localization:text("wolfhud_dialog_force_start_msg"))
-										game_state_machine:current_state():start_game_intro()
-									end,
-								},
-								[2] = {
-									text = managers.localization:text("dialog_no"),
-									is_cancel_button = true,
-								}
-							}
-							QuickMenu:new( managers.localization:text("wolfhud_dialog_force_start_title"), managers.localization:text("wolfhud_dialog_force_start_desc"), menu_options, true )
-						end
-					end
-				else
-					force_ready_clicked = 1
-					force_ready_start_t = t
-				end
-			end
-		end
-	end
 
     local ability_radial = HUDManager.set_teammate_ability_radial
     function HUDManager:set_teammate_ability_radial(i, data)
